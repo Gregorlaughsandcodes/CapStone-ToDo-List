2cf53bf69f121a88e27dd3586a616f8ebc24669f
Gregor Balak <balakgregor@gmail.com>
2023-08-26 15:24:18 +0200

    notes get add to the list

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url)); // dirname wird mit den 3 oberen Zeilen "erstellt"
 
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.static('public'));
 
 app.get("/", function(req,res){
     res.render("index.ejs");
     // res.sendFile(__dirname+ "/views/index.ejs")
+});
+
+app.post("/",function(req,res){
+    console.log(req.body["note"]);
+    res.render("index.ejs", 
+    { addNote: req.body["note"] });
 });
 
 /* app.post("/", function(req,res){
