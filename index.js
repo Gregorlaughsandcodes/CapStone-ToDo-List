87eee075a319b242a2c4e9ddf2276974f19586ca
Gregor Balak <balakgregor@gmail.com>
2023-08-28 15:22:15 +0200

    Adding ToDos on the list

--- index.js
+++ index.js
@@ -10,21 +10,29 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(express.static('public'));
+app.use(express.static('public/styles'));
+
+
+const noteArray = [];
+
 
 app.get("/", function(req,res){
     res.render("index.ejs");
     // res.sendFile(__dirname+ "/views/index.ejs")
 });
 
-app.post("/",function(req,res){
-    console.log(req.body["note"]);
-    res.render("index.ejs", 
-    { addNote: req.body["note"] });
-});
 
-/* app.post("/", function(req,res){
-   res.render("/views/index.ejs");
-}) */
+app.post("/",function(req,res){
+
+    console.log(req.body["note"]);
+
+    noteArray.push(req.body["note"]);
+
+    console.log(noteArray);
+
+    res.render("index.ejs", {notes: noteArray});
+
+});
 
 app.listen(3000,function(req,res){
     console.log("Server running on port 3000.");
